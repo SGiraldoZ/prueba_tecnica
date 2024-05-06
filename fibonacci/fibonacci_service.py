cede3450d0af35311e1cd6412bb81357deec9bdc
Sebas <sebasgizu@gmail.com>
2024-05-06 11:16:54 -0500

    fibonacci_service.py

--- fibonacci/fibonacci_service.py
+++ fibonacci/fibonacci_service.py
@@ -12,11 +12,16 @@ def fibonacci_gen(seed1,seed2):
 
 
 def fibonacci_from_time():
+    """
+    Generate a Fibonacci type sequence from the current time. Using the digits of the minutes as seeds.
+    And the number for the required number of elements
+    """
     gen_time = datetime.now().time()
     minute = str(gen_time.minute)
     second = gen_time.second
     seed1, seed2 = int(minute[-2]) if len(minute) > 1 else 0, int(minute[-1])
     fib_gen = fibonacci_gen(seed1, seed2)
+    # Second + 2 to exclude the 2 seeds
     return ([next(fib_gen) for _ in range(second+2)], gen_time.strftime("%H:%M:%S"))
         
 
