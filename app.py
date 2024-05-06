3741e82c09119682223c9ad72064364b90029802
Sebas <sebasgizu@gmail.com>
2024-05-06 10:47:29 -0500

    Remove debug from app run

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ app.register_blueprint(Fibonacci_bp, url_prefix="/fibonacci_hora")
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', port=80)
