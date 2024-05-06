2ace4a91c3a07e90dcf2f060d060ced779f970e9
Sebas <sebasgizu@gmail.com>
2024-05-06 11:10:08 -0500

    Change to exclude 2 seeds from second count of items

--- fibonacci/fibonacci_service.py
+++ fibonacci/fibonacci_service.py
@@ -17,7 +17,7 @@ def fibonacci_from_time():
     second = gen_time.second
     seed1, seed2 = int(minute[-2]) if len(minute) > 1 else 0, int(minute[-1])
     fib_gen = fibonacci_gen(seed1, seed2)
-    return ([next(fib_gen) for _ in range(second)], gen_time.strftime("%H:%M:%S"))
+    return ([next(fib_gen) for _ in range(second+2)], gen_time.strftime("%H:%M:%S"))
         
 
     
